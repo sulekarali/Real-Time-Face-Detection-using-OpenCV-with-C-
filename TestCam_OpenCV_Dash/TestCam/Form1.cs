5de4994b33d46af08513bd38efd357a0e23822d2
sulekarali <karalisule@gmail.com>
2022-01-18 22:33:31 +0300

    Real Time Face Detection using OpenCV with C#

---
+++ TestCam_OpenCV_Dash/TestCam/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace TestCam
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture capture;
+        private Mat IMG;
+        private Image<Bgr, Byte> ColImg;
+        private Image<Gray, Byte> GrayImg;
+        
+       
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        private void processFrame(object sender, EventArgs e)
+        {
+            if (capture == null)//very important to handel excption
+            {
+                try
+                {
+                    capture = new VideoCapture(); 
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+
+            IMG = capture.QueryFrame();
+                  
+            try
+            {
+                
+                imageBox1.Image = IMG;
+                ColImg = new Image<Bgr, byte>(IMG.Bitmap);
+                                
+            	GrayImg = ColImg.Convert<Gray, Byte>();
+                imageBox2.Image = GrayImg;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Idle += processFrame;
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= processFrame;
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }    
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IMG.Save("Image" +  ".jpg");
+        }       
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+//(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)(*)
+        
+    }
+}
